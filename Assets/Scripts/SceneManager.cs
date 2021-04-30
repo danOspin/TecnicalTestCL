6c066e18e3b9799274cadab9b2872b01102fa618
Cristian Daniel Murcia Ospina <cdaniel94@gmail.com>
2021-04-30 07:20:07 -0500

    Se añade botón de escape para finalizar la aplicación.

--- Assets/Scripts/SceneManager.cs
+++ Assets/Scripts/SceneManager.cs
@@ -45,7 +45,10 @@ public class SceneManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
     }
 
     public void UpdateSLToDDManager()
